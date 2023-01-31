bf27bb324a5c1250a4b36a14dab7601a0f44c91d
M Ajith kumar <102237286+AJIMANO2002@users.noreply.github.com>
2023-01-30 20:44:24 -0800

    Update route.js

--- src/Component/route.js
+++ src/Component/route.js
@@ -7,21 +7,19 @@ import Product from "../Component/product";
 import Admin from "../Adminpages/admin";
 import User from "../Adminpages/user";
 import adminuser from "../Adminpages/adminuser";    
-import Card from "../Homeneed/card";
-   
 
 function Routing() {
     return (
         <>                                                        
-        <Route path="/home" component={Home} exact/>
-        <Route path="/" component={Home} exact/>
+        <Route path="/home" component={Home} />
+        <Route path="/" component={Login} exact/>
         <Route path="/register" component={Register}/>                           
         <Route path="/login" component={Login}/>               
         <Route path="/product" component={Product}/>
         <Route path="/user"  component={User}/>                
         <Route path="/admin"  component={Admin}/>                         
         <Route path="/adminuser"  component={adminuser}/>
-        <Route path="/card"  component={Card}/>
+        
      </>
     )                                                               
 }
