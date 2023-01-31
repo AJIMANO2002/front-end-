67f4abc048c060a261f7b476ccaaa3ed88a2257b
M Ajith kumar <102237286+AJIMANO2002@users.noreply.github.com>
2023-01-30 20:48:29 -0800

    Update card.js

--- src/Component/card.js
+++ src/Component/card.js
@@ -12,7 +12,7 @@ function Card() {
 
 
     useEffect(() => {
-        axios.get("https://trendzone-backend.onrender.com/productsDetails").then((response) => {
+        axios.get("http://localhost:5000/productsDetails").then((response) => {
             setData(response.data);
             console.log(response.data)
         })
@@ -23,21 +23,23 @@ function Card() {
         <>
 
 
-            <div className="row" >
-                <div className="col d-flex justify-content-evenly align-item-center">
+            <div id="pd" className="row" >
+
+                <div className="col align-item-center">
                     <div className="row row-cols-md-4 g-3 pb-2 p-5">
 
                         {data.map((productitems) => {
                             return (
 
                                 <div className="Card">
-                                    <Link to={{ pathname: "/product", data: productitems }}>
-                                        <img className="products" src={productitems.image} />
+                                    <Link to={{ pathname: "/product", data: productitems }}> 
+                                     <img className="products" src={productitems.image} />
                                         <div className="title">
+                                       
                                             <h5 className="card-title">{productitems.name} </h5>
                                             <p className="price">{productitems.price} </p>
                                             <p className="card-tittle">{productitems.offer} </p>
-                                            <button className="btn-1" >View</button>
+                                            <button className="btn-1" >Buy Now</button>
                                         </div>
                                     </Link>
                                 </div>
