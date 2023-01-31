87f6fd8d068c5bc71185fb85be1e1251bcaef562
M Ajith kumar <102237286+AJIMANO2002@users.noreply.github.com>
2023-01-30 20:36:19 -0800

    Update login.js

--- src/Homeneed/login.js
+++ src/Homeneed/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './css/login.css';
 import axios from 'axios';
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 
 
 function Login(props) {
@@ -14,46 +14,50 @@ function Login(props) {
   const handlesubmit = async (e) => {
     e.preventDefault()
     try {
-      const url = "https://trendzone-backend.onrender.com/login";
+      const url = "http://localhost:5000/login";
       const res = await axios.post(url, user);
-      localStorage.setItem("auth",JSON.stringify(res));
+      localStorage.setItem("auth", JSON.stringify(res));
       console.log(res)
-       navigate("/home")
-      
+      navigate.push("/home")
+
     } catch {
-         console.log()
+      console.log()
     }
 
   }
   return (
     <>
-      <div className="wrapper">
+      <div className="wrappers">
         <h2>Login</h2>
         <form onSubmit={handlesubmit}>
+
           <div className="input-box">
+            <label>E-mail<span className="text-danger">*</span></label>
             <input type="text"
               name="email"
               onChange={handleChange}
               placeholder="Enter your Email" required />
           </div>
-         
+
           <div className="input-box">
+            <label style={{marginTop:"15px"}}>Password<span className="text-danger">*</span></label>
             <input type="password"
               name="password"
               onChange={handleChange}
               placeholder="Enter your password"
               required />
           </div>
-         
+
           <div className="input-box button">
-            <input onChange={handleChange} type="Submit" value="Continue" />
+            <input style={{marginTop:"33px"}} onChange={handleChange} type="Submit" value="Continue" />
           </div>
 
+        <h3 style={{fontSize:"20px", marginTop:"70px"}}><a href="./register">Sign Up</a></h3>  
         </form>
       </div>
     </>
 
-    
+
   )
 }
 export default Login
